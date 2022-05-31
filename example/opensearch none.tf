7b23abcd7f641bdc966e5a5800a09c582ee88ec8
olatte3 <pill303@naver.com>
2022-05-31 11:23:26 +0900

    -

--- example/opensearch none.tf
+++
@@ -1,9 +0,0 @@
-# module "opensearch_example" {
-#   source = "../"
-#   # aos_domain_name = "terraform-domain"
-#   # aos_data_instance_count = 1
-#   # aos_data_instance_type = "t3.small.elasticsearch"
-#   # aos_data_instance_storage = 10
-#   # aos_master_instance_count = 0
-#   # aos_master_instance_type = "t3.small.elasticsearch"
-# }
--- example/outputs.tf
+++
@@ -1,7 +0,0 @@
-# output "opensearch_domain_endpoint" {
-#   value = module.opensearch_example.opensearch_domain_endpoint
-# }
-
-# output "opensearch_domain_name" {
-#   value = module.opensearch_example.opensearch_domain_name
-# }
